b4c64c2090689e2a389c2340c9babb7e460b8838
Ryan Massey <rmassey.95@gmail.com>
2023-02-17 20:42:13 -0500

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "New Message Form" });
+});
+
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.msg,
+    user: req.body.author,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
